10dde924eba9d20b0eaad5825c32638b932957d0
Korenevskiy Denis <denkoren@yandex-team.ru>
2014-09-17 17:24:15 +0400

    Universal configenerator executable. Basic functions implementation.

---
+++ universal-configenerator.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python2.7
+
+from jinja2 import Template
+import socket
+import argparse
+
+host_fqdn = socket.getfqdn()
+hostname = host_fqdn.split('.')[0]
+
+template_variables = {
+    "fqdn": host_fqdn,
+    "hostname": hostname,
+}
+
+
+def parse_unknown_arguments(args):
+    unknown_arguments = {}
+
+    for argument in args:
+        argument = argument.lstrip("\t -")
+        try:
+            arg_name, arg_value = argument.split("=")
+            arg_name = arg_name.lstrip("-")
+            unknown_arguments[arg_name] = arg_value
+        except ValueError:
+            unknown_arguments[argument] = True
+
+    return unknown_arguments
+
+
+def parse_arguments(args=None):
+    
+    parser = argparse.ArgumentParser(prog="Webserver config generator",
+                                     description="Webserver configuration generator."
+                                                 "Parses templates with Jinja2 template engine.")
+
+    parser.add_argument("--trim", "-t", action="store_false",
+                        help="Trim block's trailing newline. Enabled by default.")
+
+    parser.add_argument("template_file", action="store", type=argparse.FileType('r'),
+                        help="Template file path", )
+
+    known_options, unknown_args = parser.parse_known_args()
+
+    unknown_options = parse_unknown_arguments(unknown_args)
+
+    return known_options, unknown_options
+
+
+def main(args=None):
+    options, additional_variables = parse_arguments(args=args)
+
+    template = Template(options.template_file.read(), trim_blocks=options.trim)
+    template_variables.update(additional_variables)
+    print template.render(**template_variables)
+
+if __name__ == "__main__":
+    main()
